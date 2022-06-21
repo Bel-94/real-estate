24b97618f0cd3da6a9b8b1e6b2ae851a0b47730d
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-21 21:04:30 +0300

    create a view function for the homepage

--- realty/views.py
+++ realty/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http.response import HttpResponse, Http404
 
 # Create your views here.
+
+#function for the home page
+def index(request):
+    return redirect(request, 'main/index.html')
+
+
