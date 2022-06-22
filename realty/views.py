85cad0c9e99067b257ba95ed839f8c5ab3ae3441
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-22 12:36:39 +0300

    confi the messages and email in the settings file

--- realty/views.py
+++ realty/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.http.response import HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
 
+# imports for creating authentication
+from django.contrib.auth.models import User
+from .models import Contacts
+
 # Create your views here.
 
 #function for the home page
