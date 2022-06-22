19a10de850ccf844c9574389ceaa46a608e1b522
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-22 07:31:41 +0300

    create the authentication url path in urls for project

--- Estate/urls.py
+++ Estate/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
+from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('realty.urls'))
+    path('', include('realty.urls')),
+    path('accounts/register/', RegistrationView.as_view(success_url='/'), name='django_registration_register'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('logout/', views.LogoutView.as_view(), {"next_page": '/'}) ,
 ]
