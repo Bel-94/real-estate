de5a3987fd87161fe1dea620efcc09b98859837d
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-22 08:33:48 +0300

    move css folder to realty app

--- realty/views.py
+++ realty/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http.response import HttpResponse, Http404
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 #function for the home page
+@login_required(login_url='login')
 def index(request):
     return HttpResponse('Welcome to your dream home')
 
