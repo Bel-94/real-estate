013cd16f824a4926b7d015462dd566b1e277ecb7
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-22 14:47:12 +0300

    create the contacts class

--- realty/models.py
+++ realty/models.py
@@ -117,7 +117,19 @@ class Listing(models.Model):
         return self.name
 
 
+class Contacts(models.Model):
+    listing=models.CharField(max_length=200)
+    listing_id=models.IntegerField()
+    name=models.CharField(max_length=200)
+    email=models.CharField(max_length=100)
+    phone=models.CharField(max_length=100)
+    message=models.TextField(blank=True)
+    contact_date=models.DateTimeField(auto_now_add=True, blank=True)
+    user_id=models.IntegerField(blank=True)
 
+
+    def __str__(self):
+        return self.name
 
 
 
