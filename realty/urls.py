8148f1fb39fe677387be171939c042bb6a2eeb88
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-23 10:32:05 +0300

    create the url paths for the pages

--- realty/urls.py
+++ realty/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('<int:listing_id>', views.listing, name='listing'),
     path('search', views.search, name='search'),
     path('contact', views.contact, name='contact'),
+    path('', views.pages, name='index'),
+    path('about', views.about, name='about')
 ]
