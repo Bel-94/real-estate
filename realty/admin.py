1495964c85f782741809702aa07ced2a20d52c33
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-23 10:09:28 +0300

    add the listing class in the admins file

--- realty/admin.py
+++ realty/admin.py
@@ -17,3 +17,17 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields=('name','email','listing')
     list_per_page=25
 admin.site.register(Contacts, ContactAdmin)
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_data', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address',
+                     'city', 'state', 'zipcode', 'price')
+    list_per_page = 25
+
+
+admin.site.register(Listing, ListingAdmin)
