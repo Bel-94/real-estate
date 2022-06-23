60b0eab600145169ce5cac4efde10e45a4dc8ed6
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-23 10:06:04 +0300

    add the realtor class in the admin

--- realty/admin.py
+++ realty/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Realtor, Contacts, Listing
 
 # Register your models here.
+# This class is not necessary but I had to write and see if there is a difference anyway
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
+admin.site.register(Realtor, RealtorAdmin)
