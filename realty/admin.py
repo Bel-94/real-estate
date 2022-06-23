cad3b9a10ac18af3ca5fd3317c53abd968f454c6
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-23 12:19:02 +0300

    makemigrations and migrate

--- realty/admin.py
+++ realty/admin.py
@@ -20,13 +20,13 @@ admin.site.register(Contacts, ContactAdmin)
 
 
 class ListingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'is_published',
-                    'price', 'list_data', 'realtor')
-    list_display_links = ('id', 'title')
+    list_display = ('id', 'name', 'is_published',
+                    'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'name')
     list_filter = ('realtor',)
     list_editable = ('is_published',)
-    search_fields = ('title', 'description', 'address',
-                     'city', 'state', 'zipcode', 'price')
+    search_fields = ('name', 'description', 'address',
+                     'location', 'county', 'price')
     list_per_page = 25
 
 
